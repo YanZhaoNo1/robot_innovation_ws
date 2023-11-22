53bfc420e053c1574c1be0f37ca5bd81faec0f9b
YanZhao <2293086836@qq.com>
2023-11-22 10:48:17 +0800

    config can change

--- src/vel/src/vel_node.cpp
+++ src/vel/src/vel_node.cpp
@@ -7,19 +7,25 @@ int main(int argc, char **argv){
     ros::NodeHandle nh;
     geometry_msgs::Twist cmd_vel;
 
+    double linear_x;
+    double linear_y;
+    double linear_z;
+    double angular_x;
+    double angular_y;
+    double angular_z;
 
-    // cmd_vel.linear.x = nh.getParam("linear_x",0.1);
-    // cmd_vel.linear.y = nh.getParam("linear_y",0.0);
-    // cmd_vel.linear.z = nh.getParam("linear_z",0.0);
-    // cmd_vel.angular.x = nh.getParam("angular_x", 0.0);
-    // cmd_vel.angular.y = nh.getParam("angular_y", 0.0);
-    // cmd_vel.angular.z = nh.getParam("angular_z", 0.0);
-    cmd_vel.linear.x = 0.1;
-    cmd_vel.linear.y = 0;
-    cmd_vel.linear.z = 0;
-    cmd_vel.angular.x = 0;
-    cmd_vel.angular.y = 0;
-    cmd_vel.angular.z = 0;
+    cmd_vel.linear.x = nh.getParam("linear_x",linear_x);
+    cmd_vel.linear.y = nh.getParam("linear_y",linear_y);
+    cmd_vel.linear.z = nh.getParam("linear_z",linear_z);
+    cmd_vel.angular.x = nh.getParam("angular_x", angular_x);
+    cmd_vel.angular.y = nh.getParam("angular_y", angular_y);
+    cmd_vel.angular.z = nh.getParam("angular_z", angular_z);
+    // cmd_vel.linear.x = 0.1;
+    // cmd_vel.linear.y = 0;
+    // cmd_vel.linear.z = 0;
+    // cmd_vel.angular.x = 0;
+    // cmd_vel.angular.y = 0;
+    // cmd_vel.angular.z = 0;
 
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1000);
     ros::Rate r(30);
