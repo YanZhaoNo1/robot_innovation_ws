3ae877bb688e7fc9f8e7afddbe8ab03cefe6d960
YanZhao <2293086836@qq.com>
2023-11-25 00:44:50 +0800

    finish config load

--- TasK.md
+++ TasK.md
@@ -7,3 +7,22 @@ https://wiki.ros.org/cn/ROS/Tutorials/CreatingMsgAndSrv
 https://wiki.ros.org/cn/ROS/TutorialsWritingServiceClient%28c%2B%2B%29
 https://wiki.ros.org/cn/ROS/TutorialsExaminingServiceClient
 3. 阅读ROSwiki Tutorials 的其他相关内容
+
+# 第二次作业
+```txt
+写速度控制节点，实现机器人画圆运动，实验代码、视频上传云班课
+1. 构建功能包vel，依赖roscpp rospy geometry_msgs
+2. 创建新文件vel_node.cpp，新建节点vel_node
+3. 申请发布话题/cmd_vel，创建发布对象vel_pub
+4. 构建一个geometry_msgs/Twist类型的消息包vel_msg，用来承载要发送的速度值
+5. 开启一个while循环，不停使用vel_pub对象发送速度消息包vel_msg
+6. 连接底盘: roslaunch zoo_bringup bringup_with_imu.launch, rosrun启动速度节
+点查看结果
+
+
+编写程序获取激光雷达数据，实验代码、视频上传云班课：
+1. 构建一个新的功能包lidar，依赖：roscpp rospy sensor_msgs
+2. 在功能包中新建节点lidar_node
+3. 在主函数中申请订阅话题/scan，并设置回调函数为LidarCallback()
+4. 构建回调函数LidarCallback（），用来接收和处理雷达数据
+5. 在回调函数调用ROS_INFO()显示雷达监测的前方障碍物距离
--- src/vel/src/vel_node.cpp
+++ src/vel/src/vel_node.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv){
 
     ros::init(argc,argv,"vel_node");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("~");
     geometry_msgs::Twist cmd_vel;
 
     double linear_x;
@@ -14,18 +14,22 @@ int main(int argc, char **argv){
     double angular_y;
     double angular_z;
 
-    cmd_vel.linear.x = nh.getParam("linear_x",linear_x);
-    cmd_vel.linear.y = nh.getParam("linear_y",linear_y);
-    cmd_vel.linear.z = nh.getParam("linear_z",linear_z);
-    cmd_vel.angular.x = nh.getParam("angular_x", angular_x);
-    cmd_vel.angular.y = nh.getParam("angular_y", angular_y);
-    cmd_vel.angular.z = nh.getParam("angular_z", angular_z);
-    // cmd_vel.linear.x = 0.1;
-    // cmd_vel.linear.y = 0;
-    // cmd_vel.linear.z = 0;
-    // cmd_vel.angular.x = 0;
-    // cmd_vel.angular.y = 0;
-    // cmd_vel.angular.z = 0;
+
+    nh.param("linear_x",linear_x,0.1);
+    nh.param("linear_y",linear_y,0.0);
+    nh.param("linear_z",linear_z,0.0);
+    nh.param("angular_x", angular_x,0.0);
+    nh.param("angular_y", angular_y,0.0);
+    nh.param("angular_z", angular_z,0.0);
+
+    cmd_vel.linear.x = linear_x;
+    cmd_vel.linear.y = linear_y;
+    cmd_vel.linear.z = linear_z;
+    cmd_vel.angular.x = angular_x;
+    cmd_vel.angular.y = angular_y;
+    cmd_vel.angular.z = angular_z;
+
+    ROS_INFO("Linear X: %f", cmd_vel.linear.x);
 
     ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1000);
     ros::Rate r(30);
